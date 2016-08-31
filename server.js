e9dc4ccff06cc6bd04e0ff86ec7e03db615d1a4c
kaelcao <aquancva@gmail.com>
2016-08-31 12:11:01 +0000

    completed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' })
+var fs = require("fs");
 
 app.set('views', __dirname+"/views");
 app.set('view engine','jade');
@@ -13,8 +14,13 @@ app.get('/',(req,res) => {
     res.render('index');
 });
 app.post('/getfilesize', upload.single('file'), function (req, res, next) {
+    var size = req.file.size;
+    fs.unlink(req.file.path,function(err){
+        if (err) throw(err);
+        console.log('file deteled');
+    });
     res.json({
-        size:req.file.size
+        size
     });
 })
 
