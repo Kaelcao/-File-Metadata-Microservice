4ae25e7f9bb3219a9bdd6f9567a18fe002d0e5a9
kaelcao <aquancva@gmail.com>
2016-08-31 12:11:41 +0000

    delete the file after upload

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.post('/getfilesize', upload.single('file'), function (req, res, next) {
     var size = req.file.size;
     fs.unlink(req.file.path,function(err){
         if (err) throw(err);
-        console.log('file deteled');
     });
     res.json({
         size
