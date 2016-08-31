b2cf74ecb90c00b2d012b2cd870c9d74caa3e819
kaelcao <aquancva@gmail.com>
2016-08-31 12:06:27 +0000

    complete

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 app.set('views', __dirname+"/views");
 app.set('view engine','jade');
@@ -8,8 +10,13 @@ app.set('view engine','jade');
 app.set('port', process.env.PORT || 5000);
 
 app.get('/',(req,res) => {
-    res.render('index',{url:req.protocol + '://' + req.get('host')});
+    res.render('index');
 });
+app.post('/getfilesize', upload.single('file'), function (req, res, next) {
+    res.json({
+        size:req.file.size
+    });
+})
 
 app.listen(app.get('port'),() => {
     console.log('App is listening at port ' + app.get('port'));
